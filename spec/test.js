edb799b82948fb8690cc22b2e9866ae2238f6e5b
Giorgio Grassini <giorgiograssini@gmail.com>
2021-05-26 13:12:12 +0200

    :white_check_mark: update tests

--- spec/test.js
+++ spec/test.js
@@ -1,17 +1,23 @@
 describe("Test for myPromise", function () {
-  it("firstPromise - should return a fulfilled promise with value done", function () {
-    return firstPromise().then((res) => expect(res).toEqual("done"));
+  describe("firstPromise()", function () {
+    it("should return a fulfilled promise with value done", function () {
+      return firstPromise().then((res) => expect(res).toEqual("done"));
+    });
   });
 
-  it("secondPromise - should return a rejected promise with value done", function () {
-    return secondPromise().catch((res) => expect(res).toEqual("error"));
+  describe("secondPromise()", function () {
+    it("should return a rejected promise with value done", function () {
+      return secondPromise().catch((res) => expect(res).toEqual("error"));
+    });
   });
 
-  it("thirdPromise should return a rejected response if isError true", function () {
-    return thirdPromise(true).catch((res) => expect(res).toEqual("error"));
-  });
+  describe("thirdPromise()", function () {
+    it("should return a rejected response if isError true", function () {
+      return thirdPromise(true).catch((res) => expect(res).toEqual("error"));
+    });
 
-  it("thirdPromise should return a fulfilled response if isError false", function () {
-    return thirdPromise(false).then((res) => expect(res).toEqual("done"));
+    it("should return a fulfilled response if isError false", function () {
+      return thirdPromise(false).then((res) => expect(res).toEqual("done"));
+    });
   });
 });
